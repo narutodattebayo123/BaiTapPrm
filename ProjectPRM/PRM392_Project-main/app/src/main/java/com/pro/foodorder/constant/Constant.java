d7f46dd0b8f6601203aba510ed7d08e567afb726
dungnthe <120459107+tienDung-T1@users.noreply.github.com>
2024-07-15 08:02:47 +0700

    project full

---
+++ ProjectPRM/PRM392_Project-main/app/src/main/java/com/pro/foodorder/constant/Constant.java
@@ -0,0 +1,24 @@
+package com.pro.foodorder.constant;
+
+public interface Constant {
+
+    String GENERIC_ERROR = "General error, please try again later";
+
+    String PAGE_FACEBOOK = "fb://";
+    String LINK_FACEBOOK = "https://www.facebook.com";
+    String LINK_YOUTUBE = "https://www.youtube.com";
+    String PHONE_NUMBER = "+123456789";
+    String GMAIL = "abc@gmail.com";
+    String SKYPE_ID = "12345678";
+    String ZALO_LINK = "https://zalo.me";
+
+    String FIREBASE_URL = "https://foodorderpro-82f42-default-rtdb.firebaseio.com";
+
+    String CURRENCY = " 000 VNĐ";
+    int TYPE_PAYMENT_CASH = 1;
+    String PAYMENT_METHOD_CASH = "Tiền mặt";
+    String ADMIN_EMAIL_FORMAT = "@admin.com";
+
+    // Key Intent
+    String KEY_INTENT_FOOD_OBJECT = "food_object";
+}
